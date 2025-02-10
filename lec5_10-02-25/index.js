2ae6f557cac699348d02e450f6fa67687ddea2d6
Jashandeep <jashandeep0673.be21@chitkara.edu.in>
2025-02-10 15:44:38 +0530

    lec5_Javascript

---
+++ lec5_10-02-25/index.js
@@ -0,0 +1,82 @@
+// console.log('hello');
+
+// var a = 10;
+
+// function squareoftwonum(x){
+//     var ans=x*x;
+//     return ans;
+// }
+
+// console.log(squareoftwonum(a));
+
+// console.log(abc);
+// sumoftwo(5,10);
+// var abc = 29;
+
+// function sumoftwo(a,b) {
+//     var ans = a+b;
+//     return ans;
+// }
+
+// var majorAns=sumoftwo(5,8);
+// console.log(majorAns);
+
+// console.log(xyz);
+// console.log(sumoftwo(10,15));
+// let xyz = 12;
+
+// function sumoftwo(a,b) {
+//     let ans = a+b;
+//     return ans;
+// }
+// console.log(sumoftwo(19,27));
+
+var a = 20;
+var b = 30;
+var c = 50;
+console.log(a);
+console.log(b);
+console.log(c);
+{
+    var a = 5;
+    var b = 4;
+    var c = 3;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    {
+        var a = 24;
+        var b = 44;
+        var c = 33;
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    }
+}
+
+
+var a=20;
+var b=30;
+var c=50;
+console.log(a);
+console.log(b);
+console.log(c); 
+
+{
+    var a=5;
+    var b=4;
+    console.log(a);
+console.log(b);
+console.log(c); 
+    {
+        var a=87;
+        var c=42;
+        console.log(a);
+console.log(b);
+console.log(c); 
+    }
+}
+console.log(a);
+console.log(b);
+console.log(c); 
+
