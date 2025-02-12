de9fa70cc07254058ca35a394f78f3d586ea6feb
Jashandeep <jashandeep0673.be21@chitkara.edu.in>
2025-02-12 15:43:12 +0530

    lec7_Js

---
+++ lec7_12-02-25/script.js
@@ -0,0 +1,53 @@
+
+// console.log(1);
+// console.log(sum(10,5));
+// function sum(a,b){
+//     return a+b;
+// }
+// setTimeout(() => {
+//    console.log(4); 
+// }, 2000);
+// var start=1;
+// var end=10000000000;
+// while(start<end){
+//     start++;
+// }
+// console.log("end");
+// setInterval(() => {
+//     console.log(9);
+// }, 4000);
+// console.log("end");
+// console.log("end");
+// console.log("end");
+
+
+// function sum(x,y){
+//     console.log(x+y);
+// }
+// const a=function(){
+//     console.log(x+y);
+// }
+
+// const b=(x,y)=>x+y;
+
+// sum(10,5);
+// console.log(b(10,10));
+
+
+// xyz();
+// abc();
+// function xyz(){
+//     console.log(1);
+// }
+// var abc=function(){
+//     console.log(2);
+// }
+
+// function xyz(){
+//     console.log(1);
+// }
+// var abc=function cdf() {
+//     console.log(2);
+// }
+// xyz();
+// abc(); here an output will be 2 as the functiuon is being created in the code sec of gec because first var value is stored undefined in the memory of gec
